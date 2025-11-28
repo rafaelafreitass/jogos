c6c9c8a2d857892d8eb2d0083b53f86b01b969f7
unknown <defreitasrafaela959@gmail.com>
2025-11-28 15:06:30 -0300

    jogos

--- Assets/códigos/EnemyJump.cs
+++ Assets/códigos/EnemyJump.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class EnemyJump : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public float jumpForce;
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "Enemy") collision.gameObject.SendMessage("Jump", jumpForce);
     }
-}
+    }

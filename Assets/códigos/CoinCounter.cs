40c1856049649df782006a73127227c3701a9a08
rafaelafreitass <defreitasrafaela959@gmail.com>
2025-12-12 14:25:03 -0300

    jogos

--- Assets/códigos/CoinCounter.cs
+++ Assets/códigos/CoinCounter.cs
@@ -4,12 +4,14 @@ public class CoinCounter : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //Debug.Log("Trigger entered by: " + other.gameObject.name);
-
         if (other.CompareTag("Player"))
         {
-            //Debug.Log("Player entered the trigger!");
+            if (CoinManager.Instance != null)
+            {
+                CoinManager.Instance.AddCoins(1);
+            }
+            
             Destroy(gameObject);
         }
-    }    
+    }
 }
